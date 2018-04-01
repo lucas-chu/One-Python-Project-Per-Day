c5d1d7bcd959430817125f662135005ecd3b389b
Lucas Chu <lucas.chu2@gmail.com>
2018-04-01 18:11:54 -0400

    3/18-4/1
    Two weeks of python files.
    From now on, I will try to update daily in order to have a more regular meter of work

---
+++ Tutorial/list1.py
@@ -0,0 +1,12 @@
+quarter = [ 'January' , 'February' , 'March' ]
+# Next, add statements to individually display the value contained in each list element
+print( 'First Month :' , quarter[0] )
+print( 'Second Month :' , quarter[1] )
+print( 'Third Month :' , quarter[2] )
+# Add a statement to create a multi-dimensional list of two elements, which themselves are lists that each have three elements containing integer values
+coords = [ [ 1 , 2 , 3 ] , [ 4 , 5 , 6 ] ]
+# Now, add statements to display the values contained in two specific inner list elements
+print( '\nTop Left 0,0 :' , coords[0][0] )
+print( 'Bottom Right 1,2 :' , coords[1][2] )
+# Finally, add a statement to display just one character of a string value
+print( '\nSecond Month First Letter :' , quarter[1][0] )
---
+++ Tutorial/operators.py
@@ -0,0 +1,33 @@
+print(' ** Exponent
+        +
+        Positive
+        -
+        Negative
+        ~
+        Bitwise NOT
+        *
+        Multiplication
+        /
+        Division
+        //
+        Floor division
+        %
+        Remainder
+        +
+        Addition
+        -
+        Subtraction
+        |
+        Bitwise OR
+        ^
+        Bitwise XOR
+        &
+        Bitwise AND
+        >>
+        Bitwise right shift
+        <<
+        Bitwise left shift
+        >, >=, <, <=, ==, !=
+        Comparison
+        = , %= , /= , //= , -= , += , *= , **=
+      ")
